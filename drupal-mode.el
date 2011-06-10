a23fc5f2285765f3a72f38d784bc04c9b42057c1
Török Gábor <gabor@20y.hu>
2011-06-10 13:03:24 +0200

    Set up customization group "drupal".

--- drupal-mode.el
+++ drupal-mode.el
@@ -11,27 +11,30 @@
 
 (require 'w3m)
 
-(defvar drupal-project-directory nil
-  "Root directory of Drupal project.")
+(defgroup drupal nil
+  "Drupal Development."
+  :group 'programming)
 
-(defun drupal-project-initialize ()
-  "Initialize Drupal project."
-  (interactive)
-  (if (not drupal-project-directory)
-      (let ((dir (read-directory-name "Project root: ")))
-        (set (make-local-variable 'drupal-project-directory) dir))
-    drupal-project-directory))
+(defcustom drupal-api-url "http://api.drupal.org/"
+  "Drupal API URL."
+  :group 'drupal)
 
-(defvar drupal-api-url "http://api.drupal.org/"
-  "Drupal API URL.")
-
-(defconst drupal-hook-docstring
+(defcustom drupal-hook-docstring
   "/**
  * Implements %s()
- */
-"
+ */"
   "Documentation string for hook."
-  )
+  :group 'drupal)
+
+;; source: http://drupal.org/node/59868
+(defcustom drupal-php-style
+  '((c-offsets-alist . ((case-label . +)
+                        (arglist-intro . +) ; for FAPI arrays and DBTNG
+                        (arglist-cont-nonempty . c-lineup-math) ; for DBTNG fields and values
+                        (arglist-close . c-lineup-close-paren) ; correct arglist closing parenthesis
+                        )))
+  "Drupal coding style."
+  :group 'drupal)
 
 (defun drupal-hook-implement (hook-name)
   "Insert API code for HOOK-NAME at point."
@@ -48,15 +51,6 @@
                                     (content (buffer-substring-no-properties beg end)))
                                (replace-regexp-in-string "^function \\(hook\\)_" module-name content nil nil 1))
                            (error "Failed to fetch page.")))))))
-
-;; source: http://drupal.org/node/59868
-(defconst drupal-php-style
-  '((c-offsets-alist . ((case-label . +)
-                        (arglist-intro . +) ; for FAPI arrays and DBTNG
-                        (arglist-cont-nonempty . c-lineup-math) ; for DBTNG fields and values
-                        (arglist-close . c-lineup-close-paren) ; correct arglist closing parenthesis
-                        )))
-  "Drupal coding standard.")
 
 ;; based on sacha chua's idea
 (defun drupal-module-name ()
